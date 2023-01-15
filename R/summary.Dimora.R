411209ac30b5ff4aacf8874888502c3e43a0c791
Filippo Ziliotto <78668728+ZiliottoFilippoDev@users.noreply.github.com>
2023-01-15 17:50:07 +0100

    v0.3.3

--- R/summary.Dimora.R
+++ R/summary.Dimora.R
@@ -6,51 +6,65 @@ summary.Dimora <- function(object,...){
     cat("Residuals:\n ")
     print(summary(object$residuals))
     cat("\n")
-    assegna.stelle <- function(object){
+    assign.stars <- function(object){
       if(object<0.001) return("***")
-      else if(object>=0.001 & object<0.01) return("**")
-      else if(object>=0.01 & object<0.05) return("*")
-      else if(object>=0.05 & object<0.1) return(".")
+      else if(object>=0.001 & object<0.01) return(" **")
+      else if(object>=0.01 & object<0.05) return("  *")
+      else if(object>=0.05 & object<0.1) return("  .")
       else return(" ")
     }
-    stelle <- sapply(object$Estimate$`p-value`,assegna.stelle)
-    fitfit <- cbind(object$Estimate,stelle)
+    stars <- sapply(object$Estimate$`p-value`,assign.stars)
+    fitfit <- cbind(object$Estimate,stars)
     colnames(fitfit) <- c(colnames(object$Estimate)," ")
     cat("Coefficients:\n ")
     print(fitfit)
     cat("---\n Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1")
     cat("\n\n")
-    cat(" Residual standard error ", sd(object$residuals), " on ", length(object$residuals)-nrow(fitfit), " degrees of freedom\n")
-    cat(" Multiple R-squared:  ",object$Rsquared," Residual squared sum: ", object$RSS)
-
+    cat(" Residual standard error ",
+        round(sqrt(sum(object$residuals^2)/(length(object$residuals)-nrow(fitfit))), digits=6),
+        " on ", length(object$residuals)-nrow(fitfit), " degrees of freedom\n")
+    cat(" Multiple R-squared:  ", round(object$Rsquared, digits=6),
+        " Residual sum of squares: ", round(object$RSS, digits=6))
+    cat("\n")
     return(invisible(object))
   }
 
   if(is.list(object$residuals)==TRUE){
+
     cat("Call: (",object$type,")\n\n  ")
     print(object$call)
     cat("\n")
-    cat("Residuals:\n ")
-    print(summary(object$Res_tot))
+    cat("Residuals Series 1:\n ")
+    print(summary(object$residuals.i[[1]]))
     cat("\n")
-    assegna.stelle <- function(object){
+    cat("Residuals Series 2:\n ")
+    print(summary(object$residuals.i[[2]]))
+    cat("\n")
+    assign.stars <- function(object){
       if(object<0.001) return("***")
-      else if(object>=0.001 & object<0.01) return("**")
-      else if(object>=0.01 & object<0.05) return("*")
-      else if(object>=0.05 & object<0.1) return(".")
+      else if(object>=0.001 & object<0.01) return(" **")
+      else if(object>=0.01 & object<0.05) return("  *")
+      else if(object>=0.05 & object<0.1) return("  .")
       else return(" ")
     }
-    stelle <- sapply(object$Estimate$`p-value`,assegna.stelle)
-    fitfit <- cbind(object$Estimate,stelle)
+    stars <- sapply(object$Estimate$`p-value`,assign.stars)
+    fitfit <- cbind(object$Estimate,stars)
     colnames(fitfit) <- c(colnames(object$Estimate)," ")
     cat("Coefficients:\n ")
     print(fitfit)
     cat("---\n Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1")
     cat("\n\n")
-    cat(" Residual standard error ", sd(object$Res_tot), " on ", length(object$Res_tot)-nrow(fitfit), " degrees of freedom\n")
-    cat(" Multiple R-squared:  ",object$Rsquared," Residual squared sum: ", object$RSS)
-
+    cat(" Residual standard error Series 1:",
+        round(sqrt(sum(object$residuals.i[[1]]^2)/(length(object$residuals.i[[1]])-nrow(fitfit))), digits=6),
+        " on ", length(object$residuals.i[[1]])-nrow(fitfit), " degrees of freedom\n")
+    cat(" Residual standard error Series 2:",
+        round(sqrt(sum(object$residuals.i[[2]]^2)/(length(object$residuals.i[[2]])-nrow(fitfit))), digits=6),
+        " on ", length(object$residuals.i[[2]])-nrow(fitfit), " degrees of freedom\n")
+    cat(" Multiple R-squared:  ",round(object$Rsquared, digits=6),
+        " Residual sum of squares: ", round(object$RSS, digits=6))
+    cat("\n")
     return(invisible(object))
 
   }
 }
+
